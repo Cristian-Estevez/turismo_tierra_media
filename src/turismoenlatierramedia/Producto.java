68b7f4ebd5d8b0337092391d2ea11e44f8d9df80
unknown <you@example.comelcistdivino@gmail.com>
2021-08-29 21:14:45 -0300

    Se crea clase Producto, falta crear constructor y terminar los setters

---
+++ src/turismoenlatierramedia/Producto.java
@@ -0,0 +1,26 @@
+package turismoenlatierramedia;
+
+public class Producto {
+	
+	private String nombre;
+	private double costo;
+	private double tiempoDeduracion;
+	private TipoDeAtraccion tipoDeAtraccion;
+	
+	public double getCosto() {
+		
+		return costo;
+	}
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+	public double getTiempoDeduracion() {
+		return tiempoDeduracion;
+	}
+	public void setTiempoDeduracion(double tiempoDeduracion) {
+		this.tiempoDeduracion = tiempoDeduracion;
+	}
+	
+	
+
+}
