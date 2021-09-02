a13c5c4492877268d88fac95b97d9c19a3703c02
Cristian-Estevez <desarrolloyprog@gmail.com>
2021-09-01 22:32:59 -0300

    Terminadas todas las promos con sus respectivos Tests

--- src/turismoenlatierramedia/PromocionAxB.java
+++ src/turismoenlatierramedia/PromocionAxB.java
@@ -1,5 +1,28 @@
 package turismoenlatierramedia;
 
-public class PromocionAxB {
+import java.util.List;
+import java.util.ListIterator;
 
+public class PromocionAxB extends Promocion{
+
+	//de la forma en que está implementado este constructor, la última atraccion de la lista que recibe como parámetro es la que vien de regalo
+	
+	public PromocionAxB(String nombre, TipoDeAtraccion tipo, List<Atraccion> atraccionesIncluidas) {
+		super(nombre, tipo, atraccionesIncluidas);
+		this.costo = calcularCosto(atraccionesIncluidas);
+	}
+
+	private double calcularCosto(List<Atraccion> atraccionesIncluidas) {
+		double costo = 0;
+		ListIterator<Atraccion> iter = atraccionesIncluidas.listIterator(atraccionesIncluidas.size());
+		int count = 0;
+		while(iter.hasPrevious()) {
+			if (count == 0) {
+				iter.previous();
+				count++;
+			}
+			costo += iter.previous().getCosto();
+		}
+		return costo;
+	}
 }
---
+++ tests/turismo_tierra_media/PromocionAxBTest.java
@@ -0,0 +1,43 @@
+package turismo_tierra_media;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import turismoenlatierramedia.Atraccion;
+import turismoenlatierramedia.PromocionAxB;
+import turismoenlatierramedia.TipoDeAtraccion;
+
+public class PromocionAxBTest {
+
+	Atraccion 	minasDeTirith, abismoDeHelm, erebor;
+
+	List<Atraccion> listaDeAtraccionesA;
+	PromocionAxB packPaisajes;
+	
+	@Before
+	public void setup() {
+		
+		minasDeTirith = new Atraccion("Minas de Tirith", 5, 2.5, 25, TipoDeAtraccion.PAISAJE);
+		abismoDeHelm = new Atraccion("Abismo de Helm", 5, 2, 15, TipoDeAtraccion.PAISAJE);
+		erebor = new Atraccion("Erebor", 12, 3, 32, TipoDeAtraccion.PAISAJE);
+		
+		listaDeAtraccionesA = new ArrayList<Atraccion>();
+		listaDeAtraccionesA.add(minasDeTirith);
+		listaDeAtraccionesA.add(erebor);
+		listaDeAtraccionesA.add(abismoDeHelm);
+		
+		packPaisajes = new PromocionAxB("Pack Paisajes", TipoDeAtraccion.PAISAJE, listaDeAtraccionesA);
+	}
+
+	@Test
+	public void alConstruirseCalculaElValorCorrectamente() {
+		double valorEsperado = minasDeTirith.getCosto() + erebor.getCosto();
+		assertEquals(valorEsperado, packPaisajes.getCosto(), 0);
+	}
+
+}
