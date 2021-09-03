42675b7fe497eca866d3cf08b6ef451d22f073f7
Cristian-Estevez <desarrolloyprog@gmail.com>
2021-09-03 13:43:07 -0300

    Se agrega un test más a ConstructorDePromocionesTest

--- src/turismoenlatierramedia/PromocionAbsoluta.java
+++ src/turismoenlatierramedia/PromocionAbsoluta.java
@@ -13,5 +13,23 @@ public class PromocionAbsoluta extends Promocion {
 	public String getTipoDePromocion() {
 		return "Promocion Absoluta";
 	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		return true;
+	}
+	
+	
 	
 }
