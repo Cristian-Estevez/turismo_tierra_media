9f1b71d74a9ec9168e8be7915a34dec4660f6839
Cristian-Estevez <desarrolloyprog@gmail.com>
2021-10-30 19:39:14 -0300

    Se borran clases y código en desuso

--- src/turismoenlatierramedia/Producto.java
+++ src/turismoenlatierramedia/Producto.java
@@ -75,7 +75,6 @@ public abstract class Producto {
 	protected abstract String esPromocionOAtraccion();
 	
 	protected abstract String imprimeLoQueIncluye();
-		
 	
 	protected abstract void ocuparPlaza();
 	
